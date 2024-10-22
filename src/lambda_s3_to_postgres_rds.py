49da766871d3c77a06c57a70d201c0cf8799cf7e
Roey Zalta <roey.zalta@gmail.com>
2024-10-22 13:52:20 +0300

    created a test json for s3_to_postgres

--- src/lambda_s3_to_postgres_rds.py
+++ src/lambda_s3_to_postgres_rds.py
@@ -9,13 +9,16 @@ import os
 import json
 import time
 import psycopg2
+import datetime
 
-aws_access_key_id = os.environ['AWS_ACCESS_ID']
-aws_secret_access_key = os.environ['AWS_SECRET_KEY_VAL']
+# Initialize aws credentials
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_KEY_VAL']
 
 # Initialize S3 client outside the handler
-s3_client = boto3.client('s3', aws_access_key_id=aws_access_key_id,
-                         aws_secret_access_key=aws_secret_access_key)
+s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID,
+                         aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+
 
 # load env variables
 bucket_name = os.environ['BUCKET_NAME']
@@ -28,91 +31,62 @@ user = os.environ['DB_USER']
 password = os.environ['DB_PASSWORD']
 port = os.environ['DB_PORT']
 output_table = os.environ['DB_TABLE']
+query = f"SELECT * FROM {output_table}"
 
 
-# func to load model from s3
-def load_model_from_s3(s3_client, bucket_name, key):
-    response = s3_client.get_object(Bucket=bucket_name, Key=key)
-    body = response['Body'].read()
-    return pickle.loads(body)
+# load model pkl
+response = s3.get_object(Bucket=bucket_name, Key=f'{artifact_folder}{model_name_s3}')
+body = response['Body'].read()
+model = pickle.loads(body)
 
+# load vectorizer pkl
+response = s3.get_object(Bucket=bucket_name, Key=f'{artifact_folder}{vectorizer_name_s3}')
+body = response['Body'].read()
+vectorizer = pickle.loads(body)
 
-# Load the model and vectorizer
-model_key = os.path.join(artifact_folder, model_name_s3)
-vectorizer_key = os.path.join(artifact_folder, vectorizer_name_s3)
-
-# load them to the lambda
-model = load_model_from_s3(s3_client, bucket_name, model_key)
-vectorizer = load_model_from_s3(s3_client, bucket_name, vectorizer_key)
-
-
-# a function to detect if a message is spam using the model loaded
-def is_spam(message):
-    print(f"Input message: {message}")
-    inp_series = pd.Series([message])
-    inp_transformed = vectorizer.transform(inp_series)
-    prediction = model.predict(inp_transformed)
-    if prediction == 'spam':
+def is_spam(inp):
+    print(inp)
+    inp = pd.Series(inp)
+    inp_test = vectorizer.transform(inp)
+    inp_sonuc = model.predict(inp_test)
+    
+    if inp_sonuc == 'spam':
         return True
     else:
         return False
 
 
-def lambda_handler(event, context):
-    try:
-        # Establish connection to the Aurora PostgreSQL database
-        conn = psycopg2.connect(
-                host=host,
-                dbname=dbname,
-                user=user,
-                password=password,
-                port=port
-        )
-        
-        # Create a cursor object
-        cur = conn.cursor()
-    
-        body = event.get('body', '')
-        data = json.loads(body)
-        text = data.get('message', '')
-        prediction = is_spam(text)
-        response = {
-                "message": text,
-                "predict": prediction
-        }
-        timestamp = int(time.time())
-        
-        # Insert the data into the database
-        insert_query = """
-               INSERT INTO predictions (text, prediction, timestamp)
-               VALUES (%s, %s, %s)
-               RETURNING id;
-               """
-        cur.execute(insert_query, (text, prediction, timestamp))
-        
-        # Fetch the id of the inserted row
-        inserted_id = cur.fetchone()[0]
-        
-        # Commit the transaction
-        conn.commit()
-        
-        # Close the cursor and connection
-        cur.close()
-        conn.close()
-        # Return the result
-        return {
-                'statusCode': 200,
-                'body'      : json.dumps({
-                        'message'   : 'Data inserted successfully',
-                        'id'        : inserted_id,
-                        'text'      : text,
-                        'prediction': prediction,
-                        'timestamp' : str(timestamp)
-                })
-        }
 
-    except Exception as e:
-        return {
-                'statusCode': 500,
-                'body'      : json.dumps({'error': str(e)})
-        }
+def lambda_handler(event, context):
+    body_json = json.loads(event['body'])
+    text_value = body_json['text']
+    result = is_spam(text_value)
+    timestemp = datetime.date.today().strftime("%B %d, %Y")
+
+    print("trying to write")
+    new_row = pd.DataFrame([[text_value, result, timestemp]],
+                           columns=['text', 'prediction_result', 'current_timestamp'])
+    with psycopg2.connect("host='{}' port={} dbname='{}' user={} password={}".format(host, port, dbname, user,
+                                                                                     password)) as conn:
+        new_row.to_sql(conn, if_exists='append')
+        print("succses")
+        df = pd.read_sql_query(query, conn)
+        print(df)
+    return {
+            'statusCode': 200,
+            'body'      : json.dumps({'result': result})
+    }
+
+if __name__ == "__main__":
+    import json
+
+    # Load test event
+    with open('src/tests/test_event.json', 'r') as f:
+        test_event = json.load(f)
+
+    # Simulate context (can be an empty object for testing)
+    test_context = {}
+
+    # Call the lambda_handler function
+    result = lambda_handler(test_event, test_context)
+    print(result)
